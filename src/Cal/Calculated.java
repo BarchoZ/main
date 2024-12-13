76d8449cd57150387daaaf599f58f49ad01fec04
BarchoZ <zakstark0@gmail.com>
2024-12-13 16:47:13 +0200

    Initial commit

--- src/Cal/Calculated.java
+++ src/Cal/Calculated.java
@@ -7,19 +7,18 @@ class Calculated{
 
     static int calculated(int operand1, int operand2, char operation) {
 
-        int resultCalculated = 0;
-        if (resultCalculated == 0){
-            System.out.println("Возможно Вы ввели неверную операцию:\n введены числа(-о), превышающие параметры установленного условия\n и/или\n Вы попытались произвести деление на 0.\n Попробуйте еще раз.   ");
-        }
-
-
-        if (operation == '+' && 0 < operand1 && operand1 < 11 && 0 < operand2 && operand2 < 11) {
+        int resultCalculated = 1;
+        if (operand1 < 0 || operand2 < 0|| operand1 > 10 || operand2 > 10){
+            System.out.println("Возможно Вы ввели неверную операцию:\n > введены числа(-о), превышающие параметры установленного условия\n Попробуйте еще раз.");
+        } else if (operation == '+' && 0 < operand1 && operand1 < 11 && 0 < operand2 && operand2 < 11) {
             resultCalculated = operand1 + operand2;
             return resultCalculated;
 
 
         } else if (operation == '-' && 0 < operand1 && operand1 < 11 && 0 < operand2 && operand2 < 11) {
+
             resultCalculated = operand1 - operand2;
+
             return resultCalculated;
 
 
@@ -27,17 +26,16 @@ class Calculated{
             resultCalculated = operand1 * operand2;
             return resultCalculated;
 
-        } else if (operation == '/' && 0 < operand1 && operand1 < 11 && 0 < operand2 && operand2 < 11) {
+        } else if (operation == '/' || 0 < operand1 || operand1 < 11 || 0 < operand2 || operand2 < 11 || operand2 != 0) {
+
+
 
             try {
-
                 resultCalculated = operand1 / operand2;
-
-            } catch (ArithmeticException | InputMismatchException e) {
-                System.out.println("ArithmeticException : " + e);
-                System.out.println("Разрешены только целые ненулевые параметры");
-
+            } catch (Exception e) {
+                System.out.println("На 0 делить нельзя. Начните сначала.");
             }
+
 
             return resultCalculated;
 
